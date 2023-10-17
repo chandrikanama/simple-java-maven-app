2f10d10b7499604fdec490ab85c15d82ed67490e
chandrikanama <58563394+chandrikanama@users.noreply.github.com>
2023-10-17 08:10:47 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message  = "Hello World! I got fever";
+    private final String message  = "Hello World! I am fine";
     public App() {}
     public static void main(String[] args) {
 
